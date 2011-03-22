19e16a3e2e0e2d213f7df5ef405b02c1a167dd85
wenke <liangwenke8@gmail.com>
2011-03-23 00:02:35 +0800

    fix bug for NamedScope method 'where'

--- lib/relation_scope.rb
+++ lib/relation_scope.rb
@@ -1,12 +1,15 @@
-# This plugin just for less than 3.x.x version rails
+# This plugin just for rails version 2.3.x with named scope module
 
 module RelationScope
   def self.included(base)
     base.class_eval do
-      named_scope :where,    lambda { |conditions| { :conditions => conditions.reject { |k, v| v.blank? } } }
+      named_scope :where,    lambda { |args|
+        conditions = args.is_a?(Hash) ? args.reject { |k, v| v.blank? } : args
+        { :conditions => conditions }
+      }
       named_scope :joins,    lambda { |joins| { :joins => joins } }
       named_scope :includes, lambda { |includes| { :include => includes } }
-      named_scope :selects,  lambda { |selects| { :select => selects } }
+      named_scope :select,   lambda { |select| { :select => select } }
       named_scope :order,    lambda { |order| { :order => order } }
       named_scope :limit,    lambda { |limit| { :limit => limit } }
       named_scope :offset,   lambda { |offset| { :offset => offset } }
